8350c5d2656f6988094c66a276389e8a9dee6b7a
PK Singh <pksin@ulaval.ca>
2023-12-18 08:53:40 -0500

    Chapter_1: Simple Makefile

---
+++ ch_1/main.c
@@ -0,0 +1,12 @@
+//
+// Created by PankajPriscillaB on 2023-12-18.
+//
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc, char **argv) {
+    printf("%d %d %d %d\n", 1, 2, 3, 4);
+    return 0;
+}
+
